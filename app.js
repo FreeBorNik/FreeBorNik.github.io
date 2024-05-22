60a4325923f0e02ab118ef09071312da2e358441
FreeBorNik <125494599+FreeBorNik@users.noreply.github.com>
2024-05-22 11:00:25 +0300

    Update app.js

--- app.js
+++ app.js
@@ -7,19 +7,23 @@ tg.MainButton.color = '#2cab37';
 
 let item = "";
 
-let btn1 = document.getElementById("button");
+let btn1 = document.getElementById("btn1");
 let btn2 = document.getElementById("btn2");
 let btn3 = document.getElementById("btn3");
 let btn4 = document.getElementById("btn4");
 let btn5 = document.getElementById("btn5");
 let btn6 = document.getElementById("btn6");
 
-btn1.onclick = function({
-	        alert("Вы нажали на кнопку"); 
-		item = "1000";
-		tg.sendData(item);
-		tg.close()}
-	);
+btn1.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Вы выбрали товар 1!");
+		item = "1";
+		tg.MainButton.show();
+	}
+});
 
 btn2.addEventListener("click", function(){
 	if (tg.MainButton.isVisible) {
