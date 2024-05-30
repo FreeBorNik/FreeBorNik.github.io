dc553cd18040119d56b36a5f4f1cebdcd1fb1c66
FreeBorNik <125494599+FreeBorNik@users.noreply.github.com>
2024-05-30 22:31:55 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let tg = window.Telegram.WebApp; //получаем объект webapp теле
 tg.expand(); //расширяем на все окно  
 
 tg.MainButton.text = "Changed Text"; //изменяем текст кнопки 
-tg.MainButton.setText("Changed Text1"); //изменяем текст кнопки иначе
+tg.MainButton.setText("Отправить данные"); //изменяем текст кнопки иначе
 tg.MainButton.textColor = "#F55353"; //изменяем цвет текста кнопки
 tg.MainButton.color = "#143F6B"; //изменяем цвет бэкграунда кнопки
 tg.MainButton.setParams({"color": "#143F6B"}); //так изменяются все параметры 
@@ -17,4 +17,9 @@ btn_close.addEventListener('click', function(){ //вешаем событие н
   else{ //иначе
   	tg.MainButton.show() //показываем 
   }
+});
+
+Telegram.WebApp.onEvent('mainButtonClicked', function(){
+	tg.sendData("some string that we need to send"); 
+	//при клике на основную кнопку отправляем данные в строковом виде
 });
