82305e37d5477e9218b56dbaceac524581823b74
FreeBorNik <125494599+FreeBorNik@users.noreply.github.com>
2024-05-31 11:49:25 +0300

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ tg.MainButton.setParams({"color": "#143F6B"}); //так изменяются в�
 
 
 btn_close.addEventListener('click', function(){ //вешаем событие на нажатие html-кнопки
-	if (document.getElementById('fname').value !== null) {
+	if (trim(document.getElementById('fname').value) !== '') {
 		answer_message = "ФИО:"+'что то есть'
 			// document.getElementById('fname').value; 
 		} else {
